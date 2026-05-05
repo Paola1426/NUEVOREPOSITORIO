a0b7e226bc118f617ea27aa14af075e0011a3f6f
Paola <villegaspaola5c@gmail.com>
2026-05-05 17:37:58 -0600

    Descripción de los cambios

--- nueva.py
+++ nueva.py
@@ -1,28 +1,6 @@
-import requests
-from PIL import Image
-from io import BytesIO
+# Programa que imprime varios mensajes
 
-# URL de la API
-URL = "https://api.nasa.gov/planetary/apod"
-
-# Parámetros (agregamos la fecha que quieres)
-params = {
-    "api_key": "DEMO_KEY",
-    "date": "2022-12-12"
-}
-
-# Hacer la petición
-response = requests.get(URL, params=params)
-data = response.json()
-
-# Mostrar info
-print("Fecha:", data["date"])
-print("Título:", data["title"])
-print("Descripción:", data["explanation"])
-
-# Obtener y mostrar la imagen
-img_url = data["url"]
-img_response = requests.get(img_url)
-
-img = Image.open(BytesIO(img_response.content))
-img.show()
+print("Bienvenido al programa")
+print("Actividad individual")
+print("ITC")
+print("Hola")
